fb1499f80815fe554795bbf90c17a8c31f37306f
Scanner <scanner@apricot.com>
2013-08-18 23:56:02 -0700

    Make it more robust if we have problems converting read data.

--- astimeseries/management/commands/load_therms_from.py
+++ astimeseries/management/commands/load_therms_from.py
@@ -10,6 +10,8 @@ data. This is not going to be that useful to others except as example code.
 # system imports
 #
 import datetime
+import os.path
+import traceback
 
 # 3rd party impots
 #
@@ -56,18 +58,42 @@ class Command(BaseCommand):
         - `*args`: Expect one argument that is the name of the file.
         - `**options`:
         """
-        file_name = args[0]
+        file_name = os.path.basename(args[0])
         tz = pytz.timezone('US/Pacific')
+
+        # Create our time series if it does not already exist.  If it does
+        # exist, empty it out (because for the most part we are loading
+        # and re-loading the same data.)
+        #
         try:
-            t = TimeSeries.objecs.get(name=file_name)
+            t = TimeSeries.objects.get(name=file_name)
+            t.data.all().delete()
+        except TimeSeries.DoesNotExist, e:
+            t = TimeSeries(name=file_name)
+            t.save()
+
         temps = []
         prev_time = None
 
-        for line in open(file_name, 'r'):
-            vals = line.split(' ')
+        for idx, line in enumerate(open(args[0], 'r')):
+            line = line.strip()
 
-            when = datetime.datetime(int(vals[0]), int(vals[1]), int(vals[2]),
-                                     int(vals[3]), int(vals[4]), tzinfo=tz)
+            # Every 100 lines spit out a '.'
+            if idx % 1000 == 0:
+                self.stdout.write('.', ending = '')
+                self.stdout.flush()
+
+            vals = line.split(' ')
+            if len(vals) != 6:
+                self.stderr.write("Line %d, bad data: '%s'" % (idx,line))
+                next
+
+            try:
+                when = datetime.datetime(int(vals[0]), int(vals[1]), int(vals[2]),
+                                         int(vals[3]), int(vals[4]), tzinfo=tz)
+            except ValueError, e:
+                self.stderr.write("One of our values can not be converted: %s" % repr(vals))
+                continue
 
             # Since we are summing all the temps within a single minute we
             # have to start with the first minute..
@@ -80,15 +106,21 @@ class Command(BaseCommand):
             # attached to our time series that is the average of the temps we
             # read in this minute.
             #
-            if prev_time != when
-                t.insert(sum(temps) / len(temps), prev_time)
-                temps = []
+            if prev_time != when:
+                try:
+                    t.insert(sum(temps) / len(temps), prev_time)
+                except ZeroDivisionError,e:
+                    self.stderr.write("Unable to create datum. time: %s, temps: %s" % (prev_time, temps))
+                    raise e
+                temps = [float(vals[5])]
+                prev_time = when
+            else:
+                temps.append(float(vals[5]))
 
         # and at the end, if our array of temps is not empty then insert it
         # because it is our last set of values at the end of the file.
         if len(temps) != 0:
             t.insert(sum(temps) / len(temps), when)
-
         return
 
 
